c6d25780c1bcf4844ae1e000da30cdb07b8b3610
tracyn09 <tracyn0099@gmail.com>
2025-02-04 13:50:44 -0500

    Task 1– Customer Profile

---
+++ cc_5.js
@@ -0,0 +1,6 @@
+//Task 1
+let customer = {name: "John Doe", age: 35, email: "john.doe@gmail.com"};
+//Logging each property
+console.log(`Customer Name:, ${customer.name}`)
+console.log(`Customer Age: ${customer.age}`)
+console.log(`Customer Email: ${customer.email}`);
