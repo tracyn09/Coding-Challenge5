60e73c0010e5947f62b8b56bd1b4b1e8e4b101ee
tracyn09 <tracyn0099@gmail.com>
2025-02-04 13:52:50 -0500

    Task 2– Order Details

--- cc_5.js
+++ cc_5.js
@@ -4,3 +4,13 @@ let customer = {name: "John Doe", age: 35, email: "john.doe@gmail.com"};
 console.log(`Customer Name:, ${customer.name}`)
 console.log(`Customer Age: ${customer.age}`)
 console.log(`Customer Email: ${customer.email}`);
+
+//Task 2
+let order = {orderId: 12345, totalAmount: 150, status: "Processing",
+    //Add displayOrder() method + calling to log properties
+    displayOrder: function() { 
+        console.log(`Order ID: ${order.orderId}`)
+        console.log(`Order Total: ${order.totalAmount}`)
+        console.log(`Order Status: ${order.status}`)}
+    };
+    order.displayOrder()
